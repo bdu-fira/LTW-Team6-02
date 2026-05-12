0c2a8b4b78fc1739476d70d0f918e4a976bf301b
Nor <ng.vdat293@gmail.com>
2026-05-12 21:14:49 +0700

    proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,15 +7,19 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://troubled-ilse-dnl-b07c8b63.koyeb.app/',
+        target: 'https://web-du-lich-4pjb.onrender.com',
         changeOrigin: true,
         secure: false,
+        // THÊM ĐOẠN NÀY:
+
       },
       '/socket.io': {
-        target: 'https://troubled-ilse-dnl-b07c8b63.koyeb.app/',
+        target: 'https://web-du-lich-4pjb.onrender.com',
         changeOrigin: true,
         secure: false,
         ws: true,
+        // THÊM ĐOẠN NÀY:
+
       },
     },
   },
